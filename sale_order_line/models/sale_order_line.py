6e8e4999e6ebf69027a2d8872c5bf98cc299408d
dessanhemrayev <dessanhemrayev@yandex.ru>
2022-11-01 19:18:31 +0300

    [FIX] added new import module and added depends for compute method

--- sale_order_line/models/sale_order_line.py
+++ sale_order_line/models/sale_order_line.py
@@ -1,4 +1,5 @@
 from odoo import api,fields,models
+from odoo.tools.profiler import profile
 
 
 class SaleOrderLine(models.Model):
@@ -6,10 +7,12 @@ class SaleOrderLine(models.Model):
 
     line_number = fields.Integer(compute = "_compute_line_number")
 
+    @profile
+    @api.depends('order_id.order_line','sequence')
     def _compute_line_number(self):
         order_ids = self.mapped('order_id')
         for order in order_ids:
             count = 1
-            for line in order.order_line:
+            for line in order.order_line.sorted(lambda l:l.sequence):
                 line.line_number = count
                 count += 1
